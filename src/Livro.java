431c84f74a7c0876922885b4d09a15fa3d579df7
elfihr <luizfelipe.vor@gmail.com>
2023-05-01 19:03:57 -0300

    Initial commit

---
+++ src/Livro.java
@@ -0,0 +1,20 @@
+public class Livro {
+    private String nomeLivro;
+    private int pagina;
+
+    public String getNomeLivro() {
+        return nomeLivro;
+    }
+
+    public void setNomeLivro(String nomeLivro) {
+        this.nomeLivro = nomeLivro;
+    }
+
+    public int getPagina() {
+        return pagina;
+    }
+
+    public void setPagina(int pagina) {
+        this.pagina = pagina;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Nome do Leitor: ");
+        String nome = leia.nextLine();
+        System.out.printf("Quantidade de Livros Lidos: ");
+        int livrosLidos = leia.nextInt();
+
+        Livro [] listaLivro = new Livro[livrosLidos];
+
+        for(int cont = 0 ; cont < livrosLidos ; cont++){
+            Livro listaTot = new Livro();
+
+            //nao funciona se o nome for varias palavras
+            System.out.print("\nNome do Livro: ");
+            listaTot.setNomeLivro(leia.next());
+
+            System.out.print("Quantidade de paginas: ");
+            listaTot.setPagina(leia.nextInt());
+
+            listaLivro [cont] = listaTot;
+        }
+
+        System.out.print("\n=====LISTA DE LIVROS LIDOS=====\n");
+        System.out.print("NOME DO LEITOR: "+ nome +"\n-----LIVROS LIDOS-----\n");
+
+        for(Livro leiaLivros :listaLivro){
+            System.out.println("LIVRO: "+leiaLivros.getNomeLivro()+" Nº PAGINAS: " + leiaLivros.getPagina());
+
+        }
+
+
+    }
+}
